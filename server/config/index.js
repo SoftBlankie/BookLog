1ba6440103eedfcb49da1cfb509642c6a4e26c17
Derick Phan <dtph@ucdavis.edu>
2021-02-24 19:10:01 -0800

    added conif

---
+++ server/config/index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+// set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // error could crash process
+  throw new Error("Couldn't find .env file");
+}
+
+module.exports = {
+  port: parseInt(process.env.PORT, 10),
+
+  databaseURL: process.env.POSTGRESQL_URI,
+
+  api: {
+    prefix: '/api',
+  }
+};
