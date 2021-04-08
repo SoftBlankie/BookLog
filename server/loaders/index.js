723bad59a6b3cdf5668772ff9a2892963ca4ce3d
Derick Phan <dtph@ucdavis.edu>
2021-04-07 18:09:58 -0700

    Integrated postgres db

--- server/loaders/index.js
+++ server/loaders/index.js
@@ -1,7 +1,11 @@
 const expressLoader = require('./express');
+const postgresLoader = require('./postgres');
 const Logger = require('./logger');
 
 module.exports = async function({ expressApp }) {
+  const postgresConnection = await postgresLoader();
+  Logger.info('PostgreSQL loaded and connected');
+
   await expressLoader({ app: expressApp });
   Logger.info('Express loaded');
 };
--- server/loaders/postgres.js
+++ server/loaders/postgres.js
@@ -1,40 +1,33 @@
 const { Pool } = require('pg');
+const Logger = require('./logger');
 
-class Database {
-  constructor() {
-    this._pool = new Pool({
-      user: 'dbuser',
-      host: 'booklog.server.com',
-      database: 'booklog-db',
-      password: 'secretpassword',
-      port: 5432,
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'booklog.server.com',
+  database: 'booklog_db',
+  password: 'secretpassword',
+  port: 5432,
+});
+
+pool.on('error', (err, client) => {
+  Logger.error(`Unexpected error on idle PostgreSQL client : ${err}`);
+});
+
+const connection = pool.connect((err, client, done) => {
+    if (err) throw err;
+    const params = args.length === 2 ? args[0] : [];
+    const callback = args.length === 1 ? args[0] : args[1];
+
+    client.query(query, params, (err, res) => {
+      done();
+      if (err) {
+        Logger.error(err.stack);
+        return callback({ error: 'Database error.'}, null);
+      }
+      callback({}, res.rows);
     });
+});
 
-    this._pool.on('error', (err, client) => {
-      Logger.error(`Unexpected error on idle PostgreSQL client : ${err}`);
-    });
-  }
-
-  query(query, ...args) {
-    this._pool.connect((err, client, done) => {
-      if (err) throw err;
-      const params = args.length === 2 ? args[0] : [];
-      const callback = args.length === 1 ? args[0] : args[1];
-
-      client.query(query, params, (err, res) => {
-        done();
-        if (err) {
-          Logger.error(err.stack);
-          return callback({ error: 'Database error.'}, null);
-        }
-        callback({}, res.rows);
-      });
-    });
-  }
-
-  end() {
-    this._pool.end();
-  }
+module.exports = () => {
+  return connection;
 }
-
-module.exports = new Database();
